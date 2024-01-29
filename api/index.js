ad05d4c1135b7615ac2157e651b5143334389e7f
no5no6 <work_yuanyang@163.com>
2024-01-29 15:13:58 +0800

    更改端口好

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const models  = require("../models");
-const port = 3000
+const port = 3100
 
 app.get('/test', (req, res) => {
   res.send('Hello World!')
--- models/index.js
+++ models/index.js
@@ -5,7 +5,8 @@ const UserSchema = require('./user')
 main().then(()=> console.log('连接成功')).catch(err => console.log('errooooooo------------',err));
 
 async function main() {
-  console.log()
+  console.log(process.env, '++++++++')
+  console.log(process.env.USERNAME, 'process.env.USERNAME')
   await mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@no5no6.6ekwral.mongodb.net/?retryWrites=true&w=majority'`)
   console.log('111111')
 }
