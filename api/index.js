39d20e5142638c6c5f478aa7a01597255ccf243a
no5no6 <work_yuanyang@163.com>
2024-01-29 20:06:28 +0800

    删除 app.listen，本地调试成功连接数据库

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
 const models  = require("../models");
-const port = 3100
+const port = process.env.PORT | 3001
 
 app.get('/test', (req, res) => {
   res.send('Hello World!')
 })
 
+module.exports = app;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
--- models/index.js
+++ models/index.js
@@ -5,10 +5,8 @@ const UserSchema = require('./user')
 main().then(()=> console.log('连接成功')).catch(err => console.log('errooooooo------------',err));
 
 async function main() {
-  console.log(process.env, '++++++++')
-  console.log(process.env.USERNAME, 'process.env.USERNAME')
   await mongoose.connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@no5no6.6ekwral.mongodb.net/?retryWrites=true&w=majority'`)
-  console.log('111111')
+  console.log('vercel dev --bug')
 }
 
 const User = mongoose.model('User', UserSchema);
