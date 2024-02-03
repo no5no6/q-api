e5540cf650f126f31a7cf0f8f71e578c188a3af1
no5no6 <work_yuanyang@163.com>
2024-02-04 00:07:43 +0800

    完成模版删除接口

--- router/questionnaireTemplate/remove.js
+++ router/questionnaireTemplate/remove.js
@@ -1,12 +1,21 @@
 const _ = require('lodash')
+
 const QuestionnaireTemplate = require('../../models/questionnaireTemplate/index')
 const { sendSuccess, sendError} = require('../../utils/responseHandler') 
 
-app.patch('/questionnaireTemplate/:id/status', (req, res) => {
+app.patch('/questionnaireTemplate/:id/status', async (req, res) => {
   const id = req.params.id
 
-  const questionnaireTemplate = this.retrieveById(req.params.id)
+  const questionnaireTemplate = await QuestionnaireTemplate.retrieveById(req.params.id)
 
-  if (questionnaireTemplate)
+  if (_.isEmpty(questionnaireTemplate)) return sendError(res, 404, '未找到需要删除的问卷模版')
+  
+  questionnaireTemplate.status = false
 
+  try {
+    const template = await questionnaireTemplate.save()
+    sendSuccess(res, template)
+  } catch (error) {
+    sendError(res, 500, '问卷模版删除失败') 
+  }
 })
